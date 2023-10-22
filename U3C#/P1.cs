7737f21d6fd296722fa003212e282708cf9223a6
ALV4R0RUIZ <146672134+ALV4R0RUIZ@users.noreply.github.com>
2023-10-21 23:08:50 -0500

    Add files via upload

---
+++ U3C#/P1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class P1
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Ingrese su edad: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n >= 18)
+            {
+                Console.WriteLine("Es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Es menor de edad");
+            }
+        }
+    }
+}
